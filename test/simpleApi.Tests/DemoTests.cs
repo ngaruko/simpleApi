e6a19588c40b25eaa93d39981e5a4305a00529f9
Bede Ngaruko <bede.ngaruko@gmail.com>
2021-06-30 08:46:27 +1200

    add some failing tests

---
+++ test/simpleApi.Tests/DemoTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace simpleApi.Tests
+{
+    public class DemoTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(35, 7*5);
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(2*5, 10);
+        }
+        
+        //Failing tests
+         [Fact]
+        public void FailingTest3()
+        {
+            Assert.Equal(35, 7*6);
+        }
+        
+        [Fact]
+        public void FailingTest4()
+        {
+            Assert.Equal(2*5, 12);
+        }
+    }
+}
